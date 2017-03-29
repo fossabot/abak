3554e77d23eb825b9f043df794c76fe5a9e7171f
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-03-29 15:09:55 +0500

    install 1.9.2 and 3.1.0.rc8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abak::Application.config.secret_token = '978f3ba375ba455c42b6c442537facb2b3f1a1224578d54a6743e85c20a0bfa585c38a7b2301644095b6def54b5ce9dca0be95e4b390f5577ec5f3fe7bd479fe'
