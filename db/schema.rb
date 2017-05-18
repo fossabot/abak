cd41ada28e62e7042a6f6706033610623763c969
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-05-18 10:57:52 +0500

    postgrSQL

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,18 +21,12 @@ ActiveRecord::Schema.define(:version => 20170401091850) do
     t.string   "slug"
   end
 
-  add_index "categories", ["slug"], :name => "index_categories_on_slug", :unique => true
-
   create_table "friendly_id_slugs", :force => true do |t|
     t.string   "slug",                         :null => false
     t.integer  "sluggable_id",                 :null => false
     t.string   "sluggable_type", :limit => 40
     t.datetime "created_at"
   end
-
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], :name => "index_friendly_id_slugs_on_slug_and_sluggable_type", :unique => true
-  add_index "friendly_id_slugs", ["sluggable_id"], :name => "index_friendly_id_slugs_on_sluggable_id"
-  add_index "friendly_id_slugs", ["sluggable_type"], :name => "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -43,8 +37,5 @@ ActiveRecord::Schema.define(:version => 20170401091850) do
     t.integer  "category_id"
     t.string   "slug"
   end
-
-  add_index "posts", ["category_id"], :name => "index_posts_on_category_id"
-  add_index "posts", ["slug"], :name => "index_posts_on_slug", :unique => true
 
 end
