6b69240e23677ebf9846e02d47919409355c97ce
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-05-18 08:36:57 +0500

    refact helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,19 @@
 # -*- encoding : utf-8 -*-
 module ApplicationHelper
-
   def build_breadcrumb(category)
-
     result = []
-
     category.ancestors.each do |ancestor|
-
-      # add content to array with link on parent and ancestors
-      result << content_tag(:li, class: "breadcrumb-item") do
+      result << content_tag(:li, class: 'breadcrumb-item') do
         link_to(ancestor.name, ancestor)
       end
-
     end
-    # add content to array current category
-    result << content_tag(:li, category.name, class: "breadcrumb-item active")
-    # return html
+    result << content_tag(:li, category.name, class: 'breadcrumb-item active')
     result.join('').html_safe
-
   end
 
   def markdown(content)
-
-    # custom renderer class for replace symbols
     renderer = CustomRenderer.new(hard_wrap: true, filter_html: true)
 
-    # options for save from smart script
     options = {
       autolink: true,
       no_intra_emphasis: true,
@@ -36,7 +24,5 @@ module ApplicationHelper
       superscript: true
     }
     Redcarpet::Markdown.new(renderer, options).render(content).html_safe
-
   end
-
 end
